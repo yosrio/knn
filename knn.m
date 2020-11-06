eb31b4a5fdb5ada1f138ec9c9eec6ef6f01cb69a
Yos Rio <yos.rio@sirclo.com>
2020-11-07 00:23:37 +0700

    knn train_test

--- knn.m
+++ knn.m
@@ -1,7 +1,18 @@
-function [ output_args ] = knn(data, k, distance)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [ output_args ] = knn(data, k, distance, trainPercent)
 
+    [x,y] = size(data);
 
+    indexTrain = ceil(x*(trainPercent/100));
+    indexTest = floor(x*((100-trainPercent)/100));
+
+    data_train_label = data(1:indexTrain,y);
+    data_train = data(1:indexTrain,1:(y-1));
+    data_test_label = data((indexTrain+1):x,y);
+    data_test = data((indexTrain+1):x,1:(y-1));
+    labelClass = unique(data_train_label);
+    
+    [xTrain,yTrain] = size(data_train);
+    [xTest,yTest] = size(data_test);
+    neighboor = [];
 end
 
